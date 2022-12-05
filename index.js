737614aec933f8da743c3e63a7349d9a8b1ba2e8
TecHAyusH6476 <ayushsingh6476@gmail.com>
2022-12-05 08:48:32 +0530

    config

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const Joi = require('joi'); 
 const app = express();
+const data=require('./data')
+const fs = require("fs");
+let request = require('request');
+
+
 app.use(express.json());
+
  
 const books = [
 {title: 'Harry Potter', id: 1},
@@ -14,7 +20,8 @@ res.send('Serving on port');
 });
  
 app.get('/api/books', (req,res)=> {
-res.send(books);
+   res.send(data)
+  
 });
  
 app.get('/api/books/:id', (req, res) => {
@@ -23,22 +30,49 @@ const book = books.find(c => c.id === parseInt(req.params.id));
 if (!book) res.status(404).send('Not valid');
 res.send(book);
 });
+
+fs.readFile("./data.json", "utf8", (err, jsonString) => {
+  if (err) {
+    console.log("Error reading file from disk:", err);
+    return;
+  }
+  try {
+    const customer = JSON.parse(jsonString);
+    console.log("Customer address is:", customer); // => "Customer address is: Infinity Loop Drive"
+  } catch (err) {
+    console.log("Error parsing JSON string:", err);
+  }
+});
  
 
 app.post('/api/books', (req, res)=> {
  
-// const { error } = validateBook(req.body);
-// if (error){
-// res.status(400).send(error.details[0].message)
-// return;
-// }
-// console.log(req.body)
-const book = {
-id: books.length + 1,
-title: req.body.title
-};
-books.push(req.body);
-res.send(req.body);
+
+  books.push(req.body.options)
+  res.send(books)
+  // let jsonData = fs.readFileSync( req.body, 'utf8');
+ 
+  // request({
+  //   method: 'POST',
+  //   url: 'http://localhost:8080/api/books',
+  //   body: jsonData,
+  //   headers: {
+  //     'User-Agent': 'Request-Promise'
+  //   },
+  //   json: true
+  // }, function (error, response, body) {
+  //   // if (error) {
+  //   //   console.log('error', error);
+  //   // }
+  
+   
+  //  res.send(body)
+  // });
+
+// res.send(req.body)
+
+// books.push(JSON.stringify(req));
+// res.send(JSON.stringify(req));
 });
  
 
